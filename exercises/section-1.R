cc425567513c29bcd7735fba8613ce48ac698d62
Lucas Mendes <lucas.lmmf@gmail.com>
2018-05-25 16:19:13 -0300

    exe 01 remaining

--- exercises/section-1.R
+++ exercises/section-1.R
@@ -27,3 +27,46 @@ View(part4)
 #e. subjects who have a loan and a balance over 1000
 part4 <- bankloanDB[bankloanDB$loan == "yes" & bankloanDB$balance > 1000,]
 View(part4)
+
+
+#Exercise #2
+#Using the file bankloan.csv:
+bankloanDB = read.csv("/Users/lucas/Documents/cursos/Statistics_with_R/csv-data-frames/bankloan.csv")
+View(bankloanDB)
+
+#a. recode the variable marital as follows: married ??? 1, single ??? 0 
+
+#bankloanDB$marital = factor(bankloanDB$marital)
+#bankloanDB$marital[bankloanDB$marital == "married"] = "1"
+#bankloanDB$marital[bankloanDB$marital == "single"] = "0"
+bankloanDB$marital2 = revalue(bankloanDB$marital, c("married"="1", "single"="0"))
+
+#b. recode the variable loan as follows: yes ??? 1, no ??? 0
+bankloanDB$loan2 = revalue(bankloanDB$loan, c("yes"="1", "no"="0"))
+
+#c. recode the variable balance as follows: under 500 ??? 1, between 501 and 1000 ??? 2, over 1000 ??? 3.
+#demo$incat2 = cut(demo$income, breaks=c(-Inf, 150, 300, Inf), labels=c("Low income", "Medium income", "High income"))
+bankloanDB$balance2 = cut(bankloanDB$balance, breaks=c(-Inf, 500, 1000, Inf), labels=c("1", "2", "3"))
+View(bankloanDB)
+
+#Exercise #3
+#Sort the data frame bankloan:
+bankloanDB = read.csv("/Users/lucas/Documents/cursos/Statistics_with_R/csv-data-frames/bankloan.csv")
+View(bankloanDB)
+
+#a. by age, ascending
+bl2 <- bankloanDB[order(bankloanDB$age),]
+
+#b. by balance, descending
+bl2 <- bankloanDB[order(-bankloanDB$balance),]
+
+#c. by age descending and by balance ascending
+bl2 <- bankloanDB[order(-bankloanDB$age, bankloanDB$balance),]
+View(bl2)
+
+#Exercise #4
+#In the data file brand.csv, create a new variable by averaging 
+#the following variables: quality, problems and ads.
+brandBD = read.csv("/Users/lucas/Documents/cursos/Statistics_with_R/csv-data-frames/brand.csv")
+brandBD$avg = (brandBD$quality + brandBD$problems + brandBD$ads) / 3
+View(brandBD)
