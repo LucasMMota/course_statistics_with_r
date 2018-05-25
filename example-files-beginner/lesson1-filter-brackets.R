f29e9cd46d39697b927741cb0e5e7698f02b2500
Lucas Mendes <lucas.lmmf@gmail.com>
2018-05-25 11:14:44 -0300

    lesson 1

--- example-files-beginner/lesson1-filter-brackets.R
+++ example-files-beginner/lesson1-filter-brackets.R
@@ -1,9 +1,7 @@
-demo <- read.csv("demographics.csv")
-
+demo <- read.csv("/Users/lucas/Documents/cursos/Statistics_with_R/csv-data-frames/demographics.csv")
 View(demo)
 
 ### how to filter (select) your data data in base R using brackets 
-
 ### a new data frame, demo2, will be created each time
 
 ######
@@ -11,29 +9,21 @@ View(demo)
 ######
 
 ### select the female subjects 
-
 demo2 <- demo[demo$gender == "Female",]
-
 View(demo2)
 
 ### retain the subjects with the income greater than 100
-
 demo2 <- demo[demo$income > 100,]
-
 View(demo2)
 
 ### if you want to keep only the variables 1, 3 and 7
 ### (age, income and gender)
-
 demo2 <- demo[demo$income > 100, c(1,3,7)]
-
 View(demo2)
 
 ### if you want to drop variables 6, 7 and 8
 ### (car category, gender and retired)
-
 demo2 <- demo[demo$income > 100, -c(6:8)]
-
 View(demo2)
 
 ######
@@ -41,7 +31,5 @@ View(demo2)
 ######
 
 ### select the female subjects with the income over 100
-
 demo2 <- demo[demo$gender == "Female" & demo$income > 100,]
-
 View(demo2)
